bea769170cf9b2c06fe3edf667857c0415a4907f
bloodstalker <thabogre@gmail.com>
2017-12-22 12:14:08 +0330

    update

--- exercise2.elm
+++ exercise2.elm
@@ -1,10 +1,23 @@
-module exercise2 exposing(..)
+module Exercise2 exposing(..)
 import Html exposing(..)
+import String exposing(..)
 
 
-String -> Int
-len_printer =
-
-
+len_printer: String -> Int -> String
+len_printer name max =
+  if String.length name > max then
+    String.toUpper name
+  else
+    name
+  
 main =
+    let 
+        name = 
+            "Farzad Sadeghi"
+        length = 
+            String.length name
+    in (len_printer name length)
+        ++ "- name length:"
+        ++ (toString length)
+        |> Html.text
 
