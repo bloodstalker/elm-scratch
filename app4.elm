c8dad1968ab8feebe762f0185d9864e6f97d6622
bloodstalker <thabogre@gmail.com>
2017-12-24 03:08:56 +0330

    update

---
+++ app4.elm
@@ -0,0 +1,37 @@
+module App4 exposing(..)
+import Html exposing (Html, div, button, text, program)
+import Html.Events exposing (onClick)
+import Random
+
+type alias Model =
+  Int
+
+init : (Model, Cmd Msg)
+init =
+  (1, Cmd.none)
+
+type Msg =
+  Roll
+  | OnResult Int
+
+view : Model -> Html Msg
+view model =
+  div []
+  [button [onClick Roll] [text "Roll"]
+  , text (toString model)
+  ]
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Roll ->
+      (model, Random.generate OnResult (Random.int 1 6))
+    OnResult res ->
+      (res, Cmd.none)
+
+main =
+  program
+  {init = init
+  , view = view
+  , update = update
+  , subscriptions = (always Sub.none)}
